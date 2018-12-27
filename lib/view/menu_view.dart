52334520eea3f04d194e049ab577389284e6945f
Pakorn Ueareeworakul <pakorn.ue@gmail.com>
2018-12-27 15:41:10 +0700

    Revert "Revert "Add video view page and skeleton code""

---
+++ lib/view/menu_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:makerthon2018/model/menu.dart';
+
+class MenuView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // make the listview page here
+  }
+}
